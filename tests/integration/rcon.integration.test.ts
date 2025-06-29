b47a03ed04539e430c622aea2324ce9e6b0490c4
hizen <134218727+hizenxyz@users.noreply.github.com>
2025-06-29 04:44:43 -0400

    test: ark integration test

--- tests/integration/rcon.integration.test.ts
+++ tests/integration/rcon.integration.test.ts
@@ -13,6 +13,19 @@ const game = Object.values(Game).includes(gameString as Game)
 
 const missing = !host || !port || !password || !game;
 
+function getCommandAndExpected(game: Game) {
+  switch (game) {
+    case Game.ARMA_REFORGER:
+      return { command: "players", expected: "Players" };
+    case Game.SEVEN_DAYS_TO_DIE:
+      return { command: "version", expected: "Version" };
+    case Game.ARK_SURVIVAL_EVOLVED:
+      return { command: "ListPlayers", expected: "Players" };
+    default:
+      return { command: "echo test", expected: "test" };
+  }
+}
+
 describe("RCON integration", () => {
   it.skipIf(missing)(
     `connects to a ${game ?? "server"}`,
@@ -27,20 +40,11 @@ describe("RCON integration", () => {
           secure,
         });
         expect(rcon).toBeInstanceOf(Rcon);
-        const command =
-          game === Game.ARMA_REFORGER
-            ? "players"
-            : game === Game.SEVEN_DAYS_TO_DIE
-              ? "version"
-              : "echo test";
-        const expectedResponse =
-          game === Game.ARMA_REFORGER
-            ? "Players on server"
-            : game === Game.SEVEN_DAYS_TO_DIE
-              ? "Version"
-              : "test";
+
+        const { command, expected } = getCommandAndExpected(game!);
+
         const response = await rcon.send(command);
-        expect(response).toContain(expectedResponse);
+        expect(response).toContain(expected);
       } finally {
         rcon?.end();
       }
